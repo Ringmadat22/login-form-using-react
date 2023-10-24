939689167d81d0746b69f381ea7c4c0e486b72d5
Ring Madat <smithemma329@gmail.com>
2023-10-24 05:25:31 +0300

    main

---
+++ login-signup/src/components/login-signup/loginSignup.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "/login-signup.css"
+
+import email_icon from "../assets/email.png"
+import user_icon  from "../assets/person.png"
+import password_icon from "../assets/password.png"
+
+const LoginSignup = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default LoginSignup
