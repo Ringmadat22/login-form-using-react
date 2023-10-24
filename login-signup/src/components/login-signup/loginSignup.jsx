d922cbb8eb4369f2fe241d92ce7721aff5da574d
Ring Madat <smithemma329@gmail.com>
2023-10-24 05:28:00 +0300

    main

--- login-signup/src/components/login-signup/loginSignup.jsx
+++ login-signup/src/components/login-signup/loginSignup.jsx
@@ -7,8 +7,27 @@ import password_icon from "../assets/password.png"
 
 const LoginSignup = () => {
     return (
-        <div>
+        <div className="container">
+            <div className="header">
+                <div className="text">Sign Up</div>
+                <div className="underline"></div>
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    <img src="" alt="" />
+                    <input type="text" />
+                </div>
 
+                <div className="input">
+                    <img src="" alt="" />
+                    <input type="text" />
+                </div>
+
+                <div className="input">
+                    <img src="" alt="" />
+                    <input type="text" />
+                </div>
+            </div>
         </div>
     )
 }
